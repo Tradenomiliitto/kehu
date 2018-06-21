c2dfe584c5d51448f932e7fcb124e6541b4cef0d
Teemu Tiilikainen <teemu.tiilikainen@gmail.com>
2018-06-21 16:22:49 +0300

    Create migration for initial schema

---
+++ migrations/20180621155036_initial_schema.js
@@ -0,0 +1,52 @@
+exports.up = function(knex, Promise) {
+  return knex.schema
+    .createTable("Users", function(table) {
+      table.increments("id").primary();
+      table.string("first_name");
+      table.string("last_name");
+      table.string("auth0_id");
+    })
+    .createTable("Tags", function(table) {
+      table.increments("id").primary();
+      table.string("text");
+    })
+    .createTable("Kehus", function(table) {
+      table.increments("id").primary();
+      table
+        .integer("giver_id")
+        .unsigned()
+        .references("id")
+        .inTable("Users");
+      table.string("giver_name");
+      table.string("location");
+      table
+        .integer("owner_id")
+        .unsigned()
+        .references("id")
+        .inTable("Users");
+      table.text("text");
+      table.string("title");
+    })
+    .createTable("Kehus_Tags", function(table) {
+      table
+        .integer("kehu_id")
+        .unsigned()
+        .references("id")
+        .inTable("Kehus")
+        .onDelete("CASCADE");
+      table
+        .integer("tag_id")
+        .unsigned()
+        .references("id")
+        .inTable("Tags")
+        .onDelete("CASCADE");
+    });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema
+    .dropTableIfExists("Kehus_Tags")
+    .dropTableIfExists("Kehus")
+    .dropTableIfExists("Tags")
+    .dropTableIfExists("Users");
+};
