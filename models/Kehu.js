e67feae101c31d5a21ff7f72f846b514bebd98d5
Teemu Tiilikainen <teemu.tiilikainen@gmail.com>
2018-06-21 16:23:29 +0300

    Create Kehu model

---
+++ models/Kehu.js
@@ -0,0 +1,69 @@
+const Model = require("objection").Model;
+const Tag = require("./Tag");
+const User = require("./User");
+
+class Kehu extends Model {
+  static get tableName() {
+    return "Kehus";
+  }
+
+  static get jsonSchema() {
+    return {
+      type: "object",
+      required: ["date_given", "giver_id", "owner_id", "text", "title"],
+      properties: {
+        id: { type: "integer" },
+        date_given: { type: "string" },
+        giver_id: { type: "integer" },
+        giver_name: { type: "string" },
+        location: { type: "string" },
+        owner_id: { type: "integer" },
+        text: { type: "string" },
+        title: { type: "string" }
+      }
+    };
+  }
+
+  static get relationMappings() {
+    return {
+      tags: {
+        relation: Model.ManyToManyRelation,
+        modelClass: Tag,
+        join: {
+          from: "Kehus.id",
+          through: {
+            from: "Kehus_Tags.kehu_id",
+            to: "Kehus_Tags.tag_id"
+          },
+          to: "Tags.id"
+        }
+      },
+      giver: {
+        relation: Model.BelongsToOneRelation,
+        modelClass: User,
+        join: {
+          from: "Users.id",
+          to: "Kehus.giver_id"
+        }
+      },
+      owner: {
+        relation: Model.BelongsToOneRelation,
+        modelClass: User,
+        join: {
+          from: "Users.id",
+          to: "Kehus.owner_id"
+        }
+      }
+    };
+  }
+
+  $beforeInsert() {
+    this.created_at = new Date().toISOString();
+  }
+
+  $beforeUpdate() {
+    this.updated_at = new Date().toISOString();
+  }
+}
+
+module.exports = Kehu;
