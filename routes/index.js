d368c60356092266c7d396807f64e917a819eca5
Teemu Tiilikainen <teemu.tiilikainen@gmail.com>
2018-06-25 17:38:38 +0300

    Initial jumbotron for home page

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,9 @@ module.exports = function setupRoutes(app) {
   app.use("/user", user);
 
   app.get("/", (req, res) => {
-    res.render("index", { user: req.user });
+    res.render("index", {
+      user: req.user,
+      kehu: {}
+    });
   });
 };
