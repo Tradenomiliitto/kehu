23d6da0a3e637ac470193c31b5e940c08987035a
Teemu Tiilikainen <teemu.tiilikainen@gmail.com>
2018-06-29 16:10:48 +0300

    Add migration to add timestamps for models

---
+++ migrations/20180629145410_add_created_at_updated_at.js
@@ -0,0 +1,25 @@
+exports.up = function(knex, Promise) {
+  return knex.schema
+    .table("Kehus", function(table) {
+      table.timestamps();
+    })
+    .table("Tags", function(table) {
+      table.timestamps();
+    })
+    .table("Users", function(table) {
+      table.timestamps();
+    });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema
+    .table("Users", function(table) {
+      table.dropTimestamps();
+    })
+    .table("Tags", function(table) {
+      table.dropTimestamps();
+    })
+    .table("Kehus", function(table) {
+      table.dropTimestamps();
+    });
+};
