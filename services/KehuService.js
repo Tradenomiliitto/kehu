e67d2dc031c4b01aa729aeb45c9f749c2211fa51
Teemu Tiilikainen <teemu.tiilikainen@gmail.com>
2018-07-09 15:31:08 +0300

    Implemented creating and updating tags in kehu

--- services/KehuService.js
+++ services/KehuService.js
@@ -1,5 +1,8 @@
-const Kehu = require("../models/Kehu");
 const moment = require("moment");
+const { transaction } = require("objection");
+const Kehu = require("../models/Kehu");
+const { findTagWithText } = require("./TagService");
+const logger = require("../logger");
 
 async function getKehus(user_id) {
   return await Kehu.query()
@@ -16,23 +19,99 @@ async function getKehu(user_id, kehu_id) {
     .first();
 }
 
-async function createKehu(data) {
-  const kehu = await Kehu.query().insert(parseKehu(data));
-  const tags = parseTags(data);
-  await Promise.all(
-    tags.map(tag => kehu.$relatedQuery("tags").insert({ text: tag }))
+async function unrelateTags(kehu, tagsFromData) {
+  const oldTags = kehu.tags;
+  const tagsToUnrelate = oldTags.filter(
+    tag =>
+      -1 ===
+      tagsFromData.findIndex(tagFromData => tag.text === tagFromData.text)
   );
-  return await Kehu.query()
-    .findById(kehu.id)
-    .eager("tags")
-    .first();
+  return await Promise.all(
+    tagsToUnrelate.map(tag =>
+      kehu
+        .$relatedQuery("tags")
+        .unrelate()
+        .where("id", tag.id)
+    )
+  );
+}
+
+async function createOrRelateTags(kehu, tagsFromData) {
+  const oldTags = kehu.tags;
+  let tagsToRelate;
+
+  if (!oldTags) {
+    tagsToRelate = tagsFromData;
+  } else {
+    tagsToRelate = tagsFromData.filter(
+      tag => -1 === oldTags.findIndex(oldTag => tag.text === oldTag.text)
+    );
+  }
+
+  return await Promise.all(
+    tagsToRelate.map(async tag => {
+      const existingTag = await findTagWithText(tag.text);
+      if (existingTag) {
+        return await kehu.$relatedQuery("tags").relate(existingTag.id);
+      } else {
+        return await kehu.$relatedQuery("tags").insert(tag);
+      }
+    })
+  );
+}
+
+async function createKehu(data) {
+  const knex = Kehu.knex();
+  let trx;
+
+  try {
+    trx = await transaction.start(knex);
+
+    const kehu = await Kehu.query().insert(parseKehu(data));
+    const tags = parseTags(data);
+    await createOrRelateTags(kehu, tags);
+    await trx.commit();
+    return await Kehu.query()
+      .findById(kehu.id)
+      .eager("tags")
+      .first();
+  } catch (error) {
+    logger.error(`Creating Kehu failed. Rolling back..`);
+    logger.error(error.message);
+    return await trx.rollback();
+  }
 }
 
 async function updateKehu(user_id, kehu_id, data) {
-  return await Kehu.query()
-    .where("owner_id", user_id)
-    .andWhere("id", kehu_id)
-    .patch(parseKehu(data));
+  const knex = Kehu.knex();
+  let trx;
+
+  try {
+    trx = await transaction.start(knex);
+
+    const result = await Kehu.query()
+      .where("owner_id", user_id)
+      .andWhere("id", kehu_id)
+      .patch(parseKehu(data));
+
+    if (!result) {
+      logger.error(`No kehu with id ${kehu_id} found for user ${user_id}`);
+      return await trx.rollback();
+    }
+
+    const tagsFromData = parseTags(data);
+    const kehu = await Kehu.query()
+      .eager("tags")
+      .where("id", kehu_id)
+      .first();
+    await unrelateTags(kehu, tagsFromData);
+    await createOrRelateTags(kehu, tagsFromData);
+    await trx.commit();
+  } catch (error) {
+    logger.error(`Updating Kehu with tags failed. Rolling back..`);
+    logger.error(error.message);
+    return await trx.rollback();
+  }
 }
 
 function parseKehu(data) {
@@ -57,7 +136,10 @@ function parseKehu(data) {
 }
 
 function parseTags(data) {
-  return data.tags.split(",");
+  return data.tags
+    .split(",")
+    .map(tag => tag.trim().toLowerCase())
+    .map(text => ({ text }));
 }
 
 module.exports = {
