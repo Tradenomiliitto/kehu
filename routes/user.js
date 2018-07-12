de2c7cc2c57afe998ecccbe0cc9a3ea601f9ca3c
Teemu Tiilikainen <teemu.tiilikainen@gmail.com>
2018-07-12 09:48:07 +0300

    Move user.pug to user dir

--- routes/user.js
+++ routes/user.js
@@ -3,7 +3,9 @@ const router = express.Router();
 const passport = require("passport");
 
 router.get("/", (req, res) => {
-  res.render("user");
+  res.render("user/index", {
+    user: req.user
+  });
 });
 
 router.get("/login", (req, res) => {
