b336bf7f288c8af2a5460be129ebdce6974a7932
Teemu Tiilikainen <teemu.tiilikainen@gmail.com>
2018-07-18 15:36:54 +0300

    Add automatic timestamps to User model

--- models/User.js
+++ models/User.js
@@ -39,6 +39,14 @@ class User extends Model {
       }
     };
   }
+
+  $beforeInsert() {
+    this.created_at = new Date().toISOString();
+  }
+
+  $beforeUpdate() {
+    this.updated_at = new Date().toISOString();
+  }
 }
 
 module.exports = User;
