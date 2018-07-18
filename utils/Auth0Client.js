d7d7ba6575ae2cc3cc14a66e0ede57754d8f9afb
Teemu Tiilikainen <teemu.tiilikainen@gmail.com>
2018-07-18 15:35:24 +0300

    Implemented Auth0Client

---
+++ utils/Auth0Client.js
@@ -0,0 +1,8 @@
+const { ManagementClient } = require("auth0");
+
+module.exports = new ManagementClient({
+  token: process.env.AUTH0_API_TOKEN,
+  domain: process.env.AUTH0_DOMAIN,
+  clientId: process.env.AUTH0_CLIENT_ID,
+  clientSecret: process.env.AUTH0_CLIENT_SECRET
+});
