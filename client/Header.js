14159508612ecb6f2af0146aee78ac0034bb2b8d
Teemu Tiilikainen <teemu.tiilikainen@gmail.com>
2018-11-11 18:42:27 +0200

    Show inline menu on wide viewports

--- client/Header.js
+++ client/Header.js
@@ -17,22 +17,30 @@ export default class Header extends Component {
 
     return (
       <header className="Header">
-        <img src="/images/kehu-logo.png" className="Header-logo" />
-        <button className="Header-menuButton" onClick={this.toggleMenu}>
-          <img src="/images/icon-menu.png" />
-        </button>
-        <div className="Header-menuContainer">
-          <menu className={menuClass}>
-            <li className="Header-menuItem">
-              <a href="/kehut">Kehut</a>
-            </li>
-            <li className="Header-menuItem">
-              <a href="/raportit">Raportit</a>
-            </li>
-            <li className="Header-menuItem">
-              <a href="/profiili">Profiili</a>
-            </li>
-          </menu>
+        <div className="container">
+          <div className="row">
+            <div className="col col-md-12 col-lg-4">
+              <img src="/images/kehu-logo.png" className="Header-logo" />
+              <button className="Header-menuButton" onClick={this.toggleMenu}>
+                <img src="/images/icon-menu.png" />
+              </button>
+            </div>
+            <div className="col col-md-12 col-lg-8">
+              <div className="Header-menuContainer">
+                <menu className={menuClass}>
+                  <li className="Header-menuItem">
+                    <a href="/kehut">Kehut</a>
+                  </li>
+                  <li className="Header-menuItem">
+                    <a href="/raportit">Raportit</a>
+                  </li>
+                  <li className="Header-menuItem">
+                    <a href="/profiili">Profiili</a>
+                  </li>
+                </menu>
+              </div>
+            </div>
+          </div>
         </div>
       </header>
     );
