b263c3f3c063f71744e9b19725bc90c2d47427f2
Teemu Tiilikainen <teemu.tiilikainen@gmail.com>
2018-11-13 18:15:19 +0200

    Implement initial Portal to show Kehu form modals

--- client/App.js
+++ client/App.js
@@ -1,18 +1,47 @@
 import React, { Component } from "react";
 import { Provider } from "react-redux";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
 import store from "./redux";
 import Header from "./Header";
 import Home from "./Home";
+import Portal from "./Portal";
 
-export default class App extends Component {
+class App extends Component {
+  static propTypes = {
+    isPortalVisible: PropTypes.bool.isRequired
+  };
+
   render() {
     return (
       <Provider store={store}>
         <div className="App">
           <Header />
           <Home />
+          {this.renderPortal()}
         </div>
       </Provider>
     );
   }
+
+  renderPortal() {
+    if (this.props.isPortalVisible) {
+      return <Portal>HELLO!</Portal>;
+    }
+  }
 }
+
+const mapStateToProps = state => ({
+  isPortalVisible: state.portal.isVisible
+});
+
+const AppContainer = connect(
+  mapStateToProps,
+  null
+)(App);
+
+export default () => (
+  <Provider store={store}>
+    <AppContainer />
+  </Provider>
+);
---
+++ client/Portal.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+
+const modalRoot = document.getElementById("portal");
+
+export default class Modal extends Component {
+  constructor(props) {
+    super(props);
+    this.el = document.createElement("div");
+  }
+
+  componentDidMount() {
+    modalRoot.appendChild(this.el);
+  }
+
+  componentWillUnmount() {
+    modalRoot.removeChild(this.el);
+  }
+
+  render() {
+    return ReactDOM.createPortal(this.props.children, this.el);
+  }
+}
