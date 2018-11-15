1a10d3b95ad85135afaa8c8c75c78a88ad077b3a
Teemu Tiilikainen <teemu.tiilikainen@gmail.com>
2018-11-16 00:10:05 +0200

    Implement WordCloudField component for kehu form

---
+++ client/components/kehuform/WordCloudField.js
@@ -0,0 +1,81 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+
+export default class WordCloudField extends Component {
+  static propTypes = {
+    className: PropTypes.string,
+    handleChange: PropTypes.func.isRequired,
+    id: PropTypes.string.isRequired,
+    label: PropTypes.string.isRequired,
+    placeholder: PropTypes.string.isRequired,
+    values: PropTypes.array.isRequired
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputValue: null
+    };
+  }
+
+  render() {
+    const { inputValue } = this.state;
+    const { className, id, label, placeholder } = this.props;
+    return (
+      <div className={`Form-group ${className}Container`}>
+        <label htmlFor={id} className="label-js">
+          {label}
+        </label>
+        <input
+          id={id}
+          className={`WordCloudField ${className}Field input-js`}
+          name={id}
+          type="text"
+          placeholder={placeholder}
+          value={inputValue}
+          onChange={this.handleInputChange}
+        />
+        <button className="Button add-js" onClick={this.handleAddClick}>
+          Lisää
+        </button>
+        {this.renderItems()}
+      </div>
+    );
+  }
+
+  renderItems() {
+    return this.props.values.map(item => {
+      return (
+        <div className="WordCloudValue item-js" key={item}>
+          {item}
+          <button
+            className="WordCloudValue-removeButton"
+            onClick={this.handleRemoveClick(item)}
+          >
+            &#10005;
+          </button>
+        </div>
+      );
+    });
+  }
+
+  handleInputChange = ({ target: { value } }) => {
+    this.setState({ inputValue: value });
+  };
+
+  handleAddClick = ev => {
+    ev.preventDefault();
+    const items = [...new Set([...this.props.values, this.state.inputValue])];
+    this.setState({ inputValue: "" }, () => {
+      this.props.handleChange(items);
+    });
+  };
+
+  handleRemoveClick = item => {
+    return ev => {
+      ev.preventDefault();
+      const items = this.props.values.filter(it => it !== item);
+      this.props.handleChange(items);
+    };
+  };
+}
---
+++ client/components/kehuform/WordCloudField.test.js
@@ -0,0 +1,116 @@
+import WordCloudField from "./WordCloudField";
+
+describe("client:components:kehuform:WordCloudField", () => {
+  let component;
+  let handleChangeStub;
+  const className = "ClassName";
+  const id = "component-id";
+  const label = "component label";
+  const placeholder = "placeholder";
+  const value1 = "value1";
+  const value2 = "value2";
+  const value3 = "value3";
+  const initValues = [];
+  const fullValues = [value1, value2];
+
+  beforeEach(() => {
+    handleChangeStub = jest.fn();
+    component = shallow(
+      <WordCloudField
+        className={className}
+        handleChange={handleChangeStub}
+        id={id}
+        label={label}
+        placeholder={placeholder}
+        values={initValues}
+      />
+    );
+  });
+
+  it("renders label", () => {
+    expect(component.find(".label-js").text()).toEqual(label);
+  });
+
+  it("renders input field", () => {
+    expect(component.find(".input-js").props()).toMatchObject({
+      className: `WordCloudField ${className}Field input-js`,
+      id,
+      name: id,
+      placeholder
+    });
+  });
+
+  it("does not render items", () => {
+    expect(component.find(".item-js").exists()).toBeFalsy();
+  });
+
+  describe("when input field changes", () => {
+    beforeEach(() => {
+      component
+        .find(".input-js")
+        .simulate("change", { target: { value: value1 } });
+    });
+
+    it("updates input value", () => {
+      changeInputValue(value1);
+    });
+
+    describe("when add button is clicked", () => {
+      beforeEach(() => {
+        addValue();
+      });
+
+      it("updates input value", () => {
+        expect(component.find(".input-js").prop("value")).toEqual("");
+      });
+
+      it("calls handleChange callback", () => {
+        expect(handleChangeStub).toBeCalledWith([value1]);
+      });
+    });
+  });
+
+  describe("when element has values", () => {
+    beforeEach(() => {
+      component.setProps({ values: fullValues });
+    });
+
+    it("renders items", () => {
+      expect(component.find(".item-js").length).toEqual(fullValues.length);
+    });
+
+    describe("when adding new value", () => {
+      it("adds new value to item list and calls handleChange callback", () => {
+        changeInputValue(value3);
+        addValue();
+        expect(handleChangeStub).toBeCalledWith([...fullValues, value3]);
+      });
+    });
+
+    describe("when trying to add same value", () => {
+      it("does not add duplicate value", () => {
+        changeInputValue(value1);
+        addValue();
+        expect(handleChangeStub).toBeCalledWith(fullValues);
+      });
+    });
+
+    describe("when removing item", () => {
+      it("removes item from list and calls handleChange callback", () => {
+        component
+          .find(".item-js button")
+          .first()
+          .simulate("click", { preventDefault: () => {} });
+        expect(handleChangeStub).toBeCalledWith([fullValues[1]]);
+      });
+    });
+  });
+
+  function changeInputValue(value) {
+    component.find(".input-js").simulate("change", { target: { value } });
+  }
+
+  function addValue() {
+    component.find(".add-js").simulate("click", { preventDefault: () => {} });
+  }
+});
