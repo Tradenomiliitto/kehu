23b680b8d6180f38657d13e4da82330e279d3770
Teemu Tiilikainen <teemu.tiilikainen@gmail.com>
2018-11-16 20:58:11 +0200

    Implement Spinner

---
+++ client/components/Spinner.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import MDSpinner from "react-md-spinner";
+
+export default class Spinner extends Component {
+  static propTypes = {
+    options: PropTypes.object
+  };
+
+  render() {
+    const defaultOptions = {
+      size: 80,
+      color1: "#ff96ac",
+      color2: "#3b5f5f",
+      color3: "#ff96ac",
+      color4: "#3b5f5f"
+    };
+    const props = Object.assign({}, defaultOptions, this.props.options);
+    return (
+      <div className="Spinner">
+        <MDSpinner {...props} />
+      </div>
+    );
+  }
+}
