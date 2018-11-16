98aefbb083cfb7bff7225991b36983e42b478507
Teemu Tiilikainen <teemu.tiilikainen@gmail.com>
2018-11-16 20:03:13 +0200

    Include kehu state in root redux state

--- client/redux/index.js
+++ client/redux/index.js
@@ -1,10 +1,12 @@
 import { createStore, combineReducers, applyMiddleware } from "redux";
 import logger from "redux-logger";
 import thunk from "redux-thunk";
+import kehu from "./kehu";
 import portal from "./portal";
 import user from "./user";
 
 const rootReducer = combineReducers({
+  kehu,
   portal,
   user
 });
