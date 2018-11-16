dd0ad58dd31356b5a00e72e6a16cece4d5eb7b39
Teemu Tiilikainen <teemu.tiilikainen@gmail.com>
2018-11-16 17:44:11 +0200

    Separate webpack config for dev and prod

---
+++ webpack.dev.config.js
@@ -0,0 +1,50 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: {
+    main: path.resolve(__dirname, "client", "index.js")
+  },
+  devtool: "source-map",
+  output: {
+    libraryTarget: "umd",
+    filename: "[name].js",
+    path: path.resolve(__dirname, "public"),
+    globalObject: "this"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "sass-loader",
+          "resolve-url-loader"
+        ]
+      },
+      {
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: "url-loader?limit=10000&mimetype=application/font-woff"
+      },
+      {
+        test: /\.(ttf|eot|svg|otf)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: "file-loader"
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css"
+    })
+  ]
+};
