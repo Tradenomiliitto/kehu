db893c614c21d0dc3ee9b219e1781f3590c4a0d6
Teemu Tiilikainen <teemu.tiilikainen@gmail.com>
2018-11-16 20:40:26 +0200

    Add get, put and delete methods to kehu api

--- routes/api/kehu.js
+++ routes/api/kehu.js
@@ -4,6 +4,15 @@ const { checkSchema, validationResult } = require("express-validator/check");
 const { kehuSchema } = require("../../utils/ValidationSchemas");
 const KehuService = require("../../services/KehuService");
 
+router.get("/", async (req, res) => {
+  try {
+    const kehus = await KehuService.getKehus(req.user.id);
+    res.json(kehus);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
 router.post("/", checkSchema(kehuSchema), async (req, res) => {
   try {
     const validations = validationResult(req);
@@ -14,6 +23,34 @@ router.post("/", checkSchema(kehuSchema), async (req, res) => {
       res.status(422).json({ errors: validations.array() });
     }
   } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+router.put("/:id", checkSchema(kehuSchema), async (req, res) => {
+  try {
+    const validations = validationResult(req);
+    if (validations.isEmpty()) {
+      const kehu = await KehuService.updateKehu(
+        req.user.id,
+        req.params.id,
+        req.body
+      );
+      res.json({ kehu });
+    } else {
+      res.status(422).json({ errors: validations.array() });
+    }
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  try {
+    await KehuService.deleteKehu(req.user.id, req.params.id);
+    res.status(203).send();
+  } catch (err) {
+    logger.error(err.stack);
     res.status(500).json({ error: err.message });
   }
 });
