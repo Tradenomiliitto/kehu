8c600a91c81efe71012badbac214ce22f83b6d93
Teemu Tiilikainen <teemu.tiilikainen@gmail.com>
2018-11-19 13:43:43 +0200

    Handle SPA routes server router

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,8 @@ const userApi = require("./api/user");
 const kehuApi = require("./api/kehu");
 const { ensureAuthenticated } = require("../utils/Middlewares");
 
+const CLIENT_ROUTES = ["/kehut"];
+
 module.exports = function setupRoutes(app) {
   app.use("/api/v1/profiili", ensureAuthenticated, userApi);
   app.use("/api/v1/kehut", ensureAuthenticated, kehuApi);
@@ -12,6 +14,16 @@ module.exports = function setupRoutes(app) {
     res.render("privacy-policy", {
       user: req.user
     });
+  });
+
+  app.get(CLIENT_ROUTES, (req, res) => {
+    if (req.user) {
+      res.render("app", {
+        csrfToken: req.csrfToken()
+      });
+    } else {
+      res.redirect("/");
+    }
   });
 
   app.get("/", async (req, res) => {
