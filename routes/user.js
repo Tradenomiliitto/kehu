41a2f12728910e86ba81dcadb832ce064fb7432f
Teemu Tiilikainen <teemu.tiilikainen@gmail.com>
2018-11-20 16:33:02 +0200

    Remove unused code

--- routes/user.js
+++ routes/user.js
@@ -1,35 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const passport = require("passport");
-const { ensureAuthenticated } = require("../utils/Middlewares");
-
-router.get("/", ensureAuthenticated, (req, res) => {
-  res.render("user/index", {
-    user: req.user
-  });
-});
-
-router.get("/vaihda-salasana", ensureAuthenticated, (req, res) => {
-  res.render("user/reset-password", {
-    env: process.env,
-    initialScreen: "forgotPassword",
-    user: req.user
-  });
-});
-
-router.get("/kirjaudu", (req, res) => {
-  if (req.user) {
-    return res.redirect("/profiili");
-  }
-  res.render("user/auth", { env: process.env });
-});
-
-router.get("/rekisteroidy", (req, res) => {
-  if (req.user) {
-    return res.redirect("/profiili");
-  }
-  res.render("user/auth", { env: process.env, initialScreen: "signUp" });
-});
 
 router.get("/kirjaudu-ulos", (req, res) => {
   req.logout();
