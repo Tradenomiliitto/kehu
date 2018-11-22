06c9d6e96e5fdefda2963d56ffa87375879109d5
Teemu Tiilikainen <teemu.tiilikainen@gmail.com>
2018-11-22 16:22:47 +0200

    Create seed for roles

---
+++ seeds/kehu_giver_roles.js
@@ -0,0 +1,17 @@
+exports.seed = function(knex, Promise) {
+  return knex("Roles")
+    .del()
+    .then(function() {
+      return knex("Roles").insert([
+        { id: 1, role: "Esimies" },
+        { id: 2, role: "Alainen" },
+        { id: 3, role: "Kollega" },
+        { id: 4, role: "Asiakas" },
+        { id: 5, role: "Lähipiiri" },
+        { id: 6, role: "Kaveri" },
+        { id: 7, role: "Opettaja" },
+        { id: 8, role: "Minä itse" },
+        { id: 9, role: "Muu" }
+      ]);
+    });
+};
