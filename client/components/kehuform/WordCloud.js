403c5c289adae0d06336537570f8b7b7630f4bfa
Teemu Tiilikainen <teemu.tiilikainen@gmail.com>
2018-11-27 20:04:53 +0200

    Implement initial WordCloud component

---
+++ client/components/kehuform/WordCloud.js
@@ -0,0 +1,57 @@
+import React, { PureComponent } from "react";
+import PropTypes from "prop-types";
+import cn from "classnames";
+import TagCloud from "react-tag-cloud";
+
+export default class WordCloud extends PureComponent {
+  static propTypes = {
+    cloudItems: PropTypes.array.isRequired,
+    handleClick: PropTypes.func.isRequired,
+    values: PropTypes.array.isRequired
+  };
+
+  render() {
+    return (
+      <div className="WordCloudContainer">
+        <div className="WordCloudWrapper">
+          <TagCloud
+            style={{
+              fontFamily: '"Work Sans", sans-serif',
+              fontSize: 10,
+              fontWeight: 300,
+              padding: 4,
+              width: "100%",
+              height: "100%"
+            }}
+            className="WordCloud"
+          >
+            {this.renderCloudItems()}
+          </TagCloud>
+        </div>
+      </div>
+    );
+  }
+
+  renderCloudItems() {
+    const { cloudItems, values } = this.props;
+    return cloudItems.map((item, i) => {
+      const classNames = cn({
+        "WordCloud-item": true,
+        "WordCloud-item--active": values.includes(item.text.toLowerCase())
+      });
+      return (
+        <div
+          key={i}
+          className={classNames}
+          onClick={this.handleItemClick(item.text)}
+        >
+          {item.text}
+        </div>
+      );
+    });
+  }
+
+  handleItemClick = text => {
+    return () => this.props.handleClick(text);
+  };
+}
