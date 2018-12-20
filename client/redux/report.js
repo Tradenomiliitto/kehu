4a472cba5dfaf83745cc35e8bdf28394d5840997
Teemu Tiilikainen <teemu.tiilikainen@gmail.com>
2018-12-20 16:58:32 +0200

    Add roles and counts to report state

--- client/redux/report.js
+++ client/redux/report.js
@@ -1,15 +1,39 @@
 import { GET_KEHUS_SUCCESS } from "./kehu";
 
 export const initialState = {
-  numberOfKehus: 0
+  numberOfKehus: 0,
+  roles: []
 };
+
+function countRoles(kehus) {
+  return kehus
+    .reduce((acc, kehu) => {
+      const index = acc.findIndex(it => it.role === kehu.role.role);
+      if (index === -1) {
+        acc.push({ role: kehu.role.role, count: 1 });
+      } else {
+        acc[index].count = acc[index].count + 1;
+      }
+      return acc;
+    }, [])
+    .sort((a, b) => {
+      if (a.count < b.count) {
+        return 1;
+      } else if (a.count > b.count) {
+        return -1;
+      } else {
+        return 0;
+      }
+    });
+}
 
 export default function reducer(state = initialState, action = {}) {
   switch (action.type) {
     case GET_KEHUS_SUCCESS:
       return {
         ...state,
-        numberOfKehus: action.payload.length
+        numberOfKehus: action.payload.length,
+        roles: countRoles(action.payload)
       };
 
     default:
--- client/redux/report.test.js
+++ client/redux/report.test.js
@@ -2,16 +2,27 @@ import { GET_KEHUS_SUCCESS } from "./kehu";
 import reducer, { initialState } from "./report";
 
 describe("client:redux:report", () => {
-  const kehus = [{}, {}, {}];
+  const kehus = [
+    { role: { role: "Esimies" } },
+    { role: { role: "Asiakas" } },
+    { role: { role: "Asiakas" } },
+    { role: { role: "Kollega" } }
+  ];
 
   describe("reducer", () => {
-    describe("on GET_KEHUS_SUCCESS", () => {
-      it("counts kehus", () => {
-        const state = initialState;
-        const action = { type: GET_KEHUS_SUCCESS, payload: kehus };
-        const expectedState = { ...state, numberOfKehus: kehus.length };
-        expect(reducer(state, action)).toEqual(expectedState);
-      });
+    it("on GET_KEHUS_SUCCESS", () => {
+      const state = initialState;
+      const action = { type: GET_KEHUS_SUCCESS, payload: kehus };
+      const expectedState = {
+        ...state,
+        numberOfKehus: kehus.length,
+        roles: [
+          { role: "Asiakas", count: 2 },
+          { role: "Esimies", count: 1 },
+          { role: "Kollega", count: 1 }
+        ]
+      };
+      expect(reducer(state, action)).toEqual(expectedState);
     });
   });
 });
