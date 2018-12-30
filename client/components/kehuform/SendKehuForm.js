08a1e84fa28ff46d60f7f71099ccff71db35d2b1
Teemu Tiilikainen <teemu.tiilikainen@gmail.com>
2018-12-30 18:22:30 +0200

    Implement initial SendKehuForm with preview

--- client/components/kehuform/SendKehuForm.js
+++ client/components/kehuform/SendKehuForm.js
@@ -1,7 +1,248 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import moment from "moment";
+import ErrorPanel from "../ErrorPanel";
+import { sendKehu } from "../../redux/kehu";
+import RoleSelectPanel from "./RoleSelectPanel";
+import TextField from "./TextField";
+import DateGivenField from "./DateGivenField";
+import WordCloudField from "./WordCloudField";
+import ReceiverNameField from "./ReceiverNameField";
+import ReceiverEmailField from "./ReceiverEmailField";
+import RoleImage from "./RoleImage";
 
-export default class SendKehuForm extends Component {
-  render() {
-    return "TODO";
+export class SendKehuForm extends Component {
+  static propTypes = {
+    sendKehu: PropTypes.func.isRequired,
+    error: PropTypes.object,
+    profile: PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      first_name: PropTypes.string.isRequired,
+      last_name: PropTypes.string.isRequired,
+      picture: PropTypes.string.isRequired
+    }).isRequired,
+    roles: PropTypes.array.isRequired,
+    situations: PropTypes.array.isRequired,
+    tags: PropTypes.array.isRequired
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      preview: false,
+      giver_id: props.profile.id,
+      giver_name: `${props.profile.first_name} ${props.profile.last_name}`,
+      receiver_name: "",
+      receiver_email: "",
+      role_id: null,
+      text: "",
+      date_given: moment(),
+      tags: [],
+      situations: []
+    };
   }
+
+  render() {
+    if (this.state.preview) {
+      return this.renderPreview();
+    }
+    return this.renderForm();
+  }
+
+  renderPreview() {
+    const {
+      receiver_name,
+      receiver_email,
+      text,
+      date_given,
+      role_id
+    } = this.state;
+    return (
+      <div className="SendKehuPreview preview-js">
+        <p className="SendKehuPreview-receiver">
+          <b>Lähetetään kehun saajalle:</b>
+          <br />
+          {receiver_name}
+          <br />
+          {receiver_email}
+        </p>
+        <div className="KehuQuoteContainer">
+          <p className="KehuQuote kehu-text-nw">{text}</p>
+        </div>
+        <p className="KehuDetails">
+          <RoleImage className="KehuDetails-image" id={role_id} />
+          <span className="kehu-giver-name-nw">{this.renderGiverName()}</span>
+          <br />
+          <span className="kehu-date-given-nw">
+            {moment(date_given).format("D.M.YYYY")}
+          </span>
+        </p>
+        <div className="KehuTags kehu-tags-nw">
+          {this.renderTagsAndSituations()}
+        </div>
+        <button
+          className="Button Button--fullWidth Button--inverse"
+          onClick={this.togglePreview}
+        >
+          Muokkaa Kehua
+        </button>
+        <button
+          className="Button Button--fullWidth send-kehu-js"
+          onClick={this.sendKehu}
+        >
+          Lähetä Kehu
+        </button>
+      </div>
+    );
+  }
+
+  renderGiverName() {
+    const { giver_name, role_id } = this.state;
+    const { roles } = this.props;
+    if (role_id) {
+      const role = roles.find(r => r.id === role_id);
+      return `${giver_name}, ${role.role.toLowerCase()}`;
+    }
+    return giver_name;
+  }
+
+  renderTagsAndSituations() {
+    const { tags, situations } = this.state;
+    return [...tags, ...situations].map((it, i) => {
+      return (
+        <span className="KehuItem" key={i}>
+          {it}
+        </span>
+      );
+    });
+  }
+
+  renderForm() {
+    const {
+      receiver_name,
+      receiver_email,
+      text,
+      date_given,
+      tags,
+      situations,
+      role_id
+    } = this.state;
+    const { roles, profile } = this.props;
+    return (
+      <form className="Form form-js" onSubmit={this.togglePreview}>
+        {this.renderErrors()}
+        <ReceiverNameField
+          value={receiver_name}
+          handleChange={this.handleChangeWithEvent("receiver_name")}
+        />
+        <ReceiverEmailField
+          value={receiver_email}
+          handleChange={this.handleChangeWithEvent("receiver_email")}
+        />
+        <div className="Form-group">
+          <label>Olen Kehun saajan:</label>
+          <RoleSelectPanel
+            selected={role_id}
+            roles={roles}
+            handleClick={this.handleRoleChange}
+          />
+        </div>
+        <TextField
+          value={text}
+          handleChange={this.handleChangeWithEvent("text")}
+        />
+        <img src={profile.picture} className="SendKehuForm-profileImage" />
+        <DateGivenField
+          value={date_given}
+          handleChange={this.handleChangeWithValue("date_given")}
+        />
+        <WordCloudField
+          id="tags"
+          className="Tags"
+          cloudItems={this.props.tags}
+          label={
+            <span>
+              Kehun asiasanat
+              <br />
+              (taidot ja ominaisuudet)
+            </span>
+          }
+          placeholder="Uusi asiasana"
+          values={tags}
+          handleChange={this.handleChangeWithValue("tags")}
+        />
+        <WordCloudField
+          id="situations"
+          className="Situation"
+          cloudItems={this.props.situations}
+          label="Kehu koskee tilannetta"
+          placeholder="Uusi tilanne"
+          values={situations}
+          handleChange={this.handleChangeWithValue("situations")}
+        />
+        <input
+          type="submit"
+          className="Button Button--fullWidth submit-kehu-nw"
+          value="Esikatsele ja lähetä"
+        />
+      </form>
+    );
+  }
+
+  renderErrors() {
+    const { error } = this.props;
+    if (error && error.responseJson && error.responseJson.errors) {
+      return error.responseJson.errors.map((e, i) => (
+        <ErrorPanel key={i} message={e.msg} />
+      ));
+    }
+  }
+
+  handleChangeWithEvent = field => {
+    return ({ target: { value } }) => {
+      this.setState({ [field]: value });
+    };
+  };
+
+  handleChangeWithValue = field => {
+    return value => {
+      this.setState({ [field]: value });
+    };
+  };
+
+  handleRoleChange = role_id => {
+    this.setState({ role_id });
+  };
+
+  togglePreview = ev => {
+    ev.preventDefault();
+    this.setState({ preview: !this.state.preview });
+  };
+
+  sendKehu = () => {
+    const formData = {
+      ...this.state,
+      date_given: moment(this.state.date_given).format()
+    };
+    delete formData.preview;
+    this.props.sendKehu(formData);
+  };
 }
+
+const mapStateToProps = state => ({
+  error: state.kehu.error,
+  profile: state.profile.profile,
+  roles: state.profile.roles,
+  situations: state.profile.situations,
+  tags: state.profile.tags
+});
+
+const mapDispatchToProps = {
+  sendKehu
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(SendKehuForm);
---
+++ client/components/kehuform/SendKehuForm.test.js
@@ -0,0 +1,130 @@
+import { SendKehuForm } from "./SendKehuForm";
+import moment from "moment";
+import MockDate from "mockdate";
+
+describe("client:components:kehuform:SendKehuForm", () => {
+  let component;
+  let sendKehuStub;
+  const profile = {
+    id: 1,
+    first_name: "First",
+    last_name: "Last",
+    picture: "picture.jpg"
+  };
+  const error1 = { msg: "error 1" };
+  const error2 = { msg: "error 2" };
+  const error = {
+    responseJson: {
+      errors: [error1, error2]
+    }
+  };
+  const roles = [{ id: 1, role: "role1" }];
+  const situations = [{ text: "situation" }];
+  const tags = [{ text: "tag" }];
+
+  beforeEach(() => {
+    MockDate.set("2018-11-17");
+    sendKehuStub = jest.fn();
+    component = shallow(
+      <SendKehuForm
+        sendKehu={sendKehuStub}
+        profile={profile}
+        roles={roles}
+        situations={situations}
+        tags={tags}
+      />
+    );
+  });
+
+  it("initialises empty state", () => {
+    expect(component.state()).toEqual({
+      preview: false,
+      giver_id: profile.id,
+      giver_name: `${profile.first_name} ${profile.last_name}`,
+      receiver_name: "",
+      receiver_email: "",
+      role_id: null,
+      text: "",
+      date_given: moment(),
+      tags: [],
+      situations: []
+    });
+  });
+
+  it("does not render ErrorPanel", () => {
+    expect(component.find("ErrorPanel").exists()).toBeFalsy();
+  });
+
+  it("does not render preview", () => {
+    expect(component.find(".form-js").exists()).toBeTruthy();
+    expect(component.find(".preview-js").exists()).toBeFalsy();
+  });
+
+  describe("when form is submitted", () => {
+    beforeEach(() => {
+      component.simulate("submit", { preventDefault: () => {} });
+    });
+
+    it("opens preview", () => {
+      expect(component.find(".preview-js").exists()).toBeTruthy();
+      expect(component.find(".form-js").exists()).toBeFalsy();
+    });
+
+    describe("when kehu is sent", () => {
+      beforeEach(() => {
+        component
+          .find(".send-kehu-js")
+          .simulate("click", { preventDefault: () => {} });
+      });
+
+      it("sends new kehu", () => {
+        expect(sendKehuStub).toHaveBeenCalledWith({
+          giver_id: profile.id,
+          giver_name: `${profile.first_name} ${profile.last_name}`,
+          receiver_name: "",
+          receiver_email: "",
+          role_id: null,
+          text: "",
+          date_given: moment().format(),
+          tags: [],
+          situations: []
+        });
+      });
+    });
+  });
+
+  describe("when errors are given", () => {
+    beforeEach(() => {
+      component = shallow(
+        <SendKehuForm
+          sendKehu={sendKehuStub}
+          profile={profile}
+          error={error}
+          roles={roles}
+          situations={situations}
+          tags={tags}
+        />
+      );
+    });
+
+    it("renders ErrorPanel for each error", () => {
+      expect(
+        component
+          .find("ErrorPanel")
+          .first()
+          .prop("message")
+      ).toEqual(error1.msg);
+      expect(
+        component
+          .find("ErrorPanel")
+          .last()
+          .prop("message")
+      ).toEqual(error2.msg);
+    });
+  });
+
+  afterEach(() => {
+    sendKehuStub.mockClear();
+    MockDate.reset();
+  });
+});
