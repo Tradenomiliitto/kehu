b8934163c128fa31dd2847293467efd2a53433d9
Teemu Tiilikainen <teemu.tiilikainen@gmail.com>
2019-01-26 20:47:34 +0200

    Implement blog post page stub

--- routes/blog.js
+++ routes/blog.js
@@ -2,6 +2,13 @@ const express = require("express");
 const router = express.Router();
 const BlogService = require("../services/BlogService");
 
+router.get("/:slug", async (req, res) => {
+  const post = await BlogService.getPost(req.params.slug);
+  res.render("blog-post", {
+    post
+  });
+});
+
 router.get("/", async (req, res) => {
   const posts = await BlogService.getPosts();
   res.render("blog", {
--- services/BlogService.js
+++ services/BlogService.js
@@ -2,6 +2,11 @@ const { client } = require("../utils/contentful");
 const logger = require("../logger");
 const { parsePosts } = require("../utils/BlogUtils");
 
+async function getPost(slug) {
+  const posts = await getPosts();
+  return posts.find(post => post.slug === slug);
+}
+
 async function getPosts() {
   try {
     const entries = await client.getEntries({
@@ -15,5 +20,6 @@ async function getPosts() {
 }
 
 module.exports = {
+  getPost,
   getPosts
 };
