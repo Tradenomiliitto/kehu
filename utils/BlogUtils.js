b09b549a6fe723beb5e0a4931f95dd7100833bcf
Teemu Tiilikainen <teemu.tiilikainen@gmail.com>
2019-01-26 20:55:04 +0200

    Include similar posts data in post

--- utils/BlogUtils.js
+++ utils/BlogUtils.js
@@ -1,6 +1,34 @@
 const { documentToHtmlString } = require("@contentful/rich-text-html-renderer");
 const moment = require("moment");
 
+function parseSimilarPost(post) {
+  return {
+    author: post.fields.author,
+    excerpt: post.fields.excerpt,
+    image: {
+      url: post.fields.header.fields.file.url
+    },
+    published: moment(post.sys.createdAt).format("D.M.YYYY"),
+    slug: post.fields.url,
+    tags: post.fields.tags,
+    title: post.fields.title
+  };
+}
+
+function getSimilarPosts(post, posts) {
+  return post.fields.tags.reduce((acc, tag) => {
+    if (acc.length === 2) {
+      return acc;
+    }
+    posts.forEach(p => {
+      if (acc.length < 2 && p.fields.tags.includes(tag)) {
+        acc.push(parseSimilarPost(p));
+      }
+    });
+    return acc;
+  }, []);
+}
+
 function parsePost(posts, post, index) {
   return {
     author: post.fields.author,
@@ -12,6 +40,7 @@ function parsePost(posts, post, index) {
     nextPost: posts[index - 1] ? posts[index - 1].fields.url : null,
     prevPost: posts[index + 1] ? posts[index + 1].fields.url : null,
     published: moment(post.sys.createdAt).format("D.M.YYYY"),
+    similarPosts: getSimilarPosts(post, posts),
     slug: post.fields.url,
     tags: post.fields.tags,
     title: post.fields.title
