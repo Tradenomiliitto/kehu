711f3d59bf01cabb4ff8e4366a43582ca433f3dd
Teemu Tiilikainen <teemu.tiilikainen@gmail.com>
2019-02-13 19:45:15 +0200

    Implement SentKehuItem component

---
+++ client/components/home/SentKehuItem.js
@@ -0,0 +1,46 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import moment from "moment";
+
+export class SentKehuItem extends Component {
+  static propTypes = {
+    kehu: PropTypes.object.isRequired,
+    roles: PropTypes.array.isRequired
+  };
+
+  render() {
+    const { kehu } = this.props;
+    return (
+      <div className="FeedItem">
+        <span className="FeedItem-date">
+          {moment(kehu.date_given).format("D.M.YYYY")}
+        </span>
+        <p className="FeedItem-text">{kehu.text}</p>
+        <p className="FeedItem-info">{this.createInfo(kehu)}</p>
+      </div>
+    );
+  }
+
+  createInfo(kehu) {
+    let text = "Lähetetty kehu: ";
+
+    if (kehu.role_id) {
+      const role = this.props.roles.find(r => r.id === kehu.role_id);
+      text += `${kehu.giver_name}, ${role.role}`;
+    } else {
+      text += kehu.giver_name;
+    }
+
+    return text;
+  }
+}
+
+const mapStateToProps = state => ({
+  roles: state.profile.roles
+});
+
+export default connect(
+  mapStateToProps,
+  null
+)(SentKehuItem);
---
+++ client/components/home/SentKehuItem.test.js
@@ -0,0 +1,41 @@
+import { SentKehuItem } from "./SentKehuItem";
+import moment from "moment";
+
+describe("client:components:home:SentKehuItem", () => {
+  let component;
+  const roles = [{ id: 1, role: "client" }, { id: 6, role: "client" }];
+  const kehu = {
+    date_given: "2019-02-10",
+    text: "text",
+    giver_name: "name"
+  };
+
+  beforeEach(() => {
+    component = shallow(<SentKehuItem kehu={kehu} roles={roles} />);
+  });
+
+  it("renders date", () => {
+    expect(component.find(".FeedItem-date").text()).toEqual(
+      moment(kehu.date_given).format("D.M.YYYY")
+    );
+  });
+
+  it("renders text", () => {
+    expect(component.find(".FeedItem-text").text()).toEqual(kehu.text);
+  });
+
+  it("renders correct info", () => {
+    const expectedInfo = `Lähetetty kehu: ${kehu.giver_name}`;
+    expect(component.find(".FeedItem-info").text()).toEqual(expectedInfo);
+  });
+
+  describe("when role is given", () => {
+    it("renders correct info", () => {
+      component.setProps({ kehu: { ...kehu, role_id: 6 } });
+      const expectedInfo = `Lähetetty kehu: ${kehu.giver_name}, ${
+        roles[1].role
+      }`;
+      expect(component.find(".FeedItem-info").text()).toEqual(expectedInfo);
+    });
+  });
+});
