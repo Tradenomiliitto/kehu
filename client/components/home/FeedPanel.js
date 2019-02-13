4583952244bae77fcbe031b9802c8728145a2891
Teemu Tiilikainen <teemu.tiilikainen@gmail.com>
2019-02-13 19:46:02 +0200

    Implement FeedPanel with item logic

--- client/components/home/FeedPanel.js
+++ client/components/home/FeedPanel.js
@@ -1,5 +1,8 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+import KehuItem from "./KehuItem";
+import SentKehuItem from "./SentKehuItem";
 
 export default class FeedPanel extends Component {
   static propTypes = {
@@ -7,6 +10,23 @@ export default class FeedPanel extends Component {
   };
 
   render() {
-    return "TODO";
+    return (
+      <div className="Feed">
+        <h1 className="Feed-title">Viimeaikainen toiminta</h1>
+        {this.renderItems()}
+        <Link to="/kehut" className="Button Feed-link">
+          Kaikki Kehut
+        </Link>
+      </div>
+    );
+  }
+
+  renderItems() {
+    return this.props.items.map((it, i) => {
+      if (it.id) {
+        return <KehuItem key={i} kehu={it} />;
+      }
+      return <SentKehuItem key={i} kehu={it} />;
+    });
   }
 }
---
+++ client/components/home/FeedPanel.test.js
@@ -0,0 +1,25 @@
+import FeedPanel from "./FeedPanel";
+import SentKehuItem from "./SentKehuItem";
+
+describe("client:components:home:FeedPanel", () => {
+  let component;
+  const items = [
+    { id: 1 },
+    { name: "name" },
+    { id: 2 },
+    { name: "other" },
+    { id: 3 }
+  ];
+
+  beforeEach(() => {
+    component = shallow(<FeedPanel items={items} />);
+  });
+
+  it("renders KehuItem for all kehus", () => {
+    expect(component.find("KehuItem").length).toEqual(3);
+  });
+
+  it("renders SentKehuItem for all sent kehus", () => {
+    expect(component.find(SentKehuItem).length).toEqual(2);
+  });
+});
