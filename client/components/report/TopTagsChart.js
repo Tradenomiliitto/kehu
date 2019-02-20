cb51fec8d1afa01602bb5ce80cc508a7d7493d4b
Teemu Tiilikainen <teemu.tiilikainen@gmail.com>
2019-02-20 18:38:21 +0200

    Replace react-svg-piechart with chart.js

---
+++ client/components/report/TopTagsChart.js
@@ -0,0 +1,47 @@
+import Chart from "chart.js";
+import "chartjs-plugin-labels";
+
+const COLORS = ["#3EABAB", "#FF96AC", "#3B5F5F", "#F3BBC8", "#FAE6EB"];
+
+export default class TopTagsChart {
+  constructor(canvas, props) {
+    const data = this.formatData(props);
+    this.chart = new Chart(canvas.getContext("2d"), {
+      data,
+      type: "pie",
+      options: {
+        color: COLORS,
+        legend: {
+          display: false
+        },
+        maintainAspectRatio: false,
+        plugins: {
+          labels: {
+            render: "label",
+            fontSize: 14,
+            fontColor: "#000",
+            fontFamily: '"Work Sans", Arial, sans-serif'
+          }
+        },
+        responsive: true,
+        tooltips: {
+          enabled: false
+        }
+      }
+    });
+  }
+
+  formatData({ tags }) {
+    return {
+      datasets: [
+        {
+          data: tags.map(t => t.count),
+          backgroundColor: COLORS
+        }
+      ],
+      labels: tags
+        .map(t => t.text)
+        .map(t => t.charAt(0).toUpperCase() + t.slice(1))
+    };
+  }
+}
--- client/components/report/TopTagsPanel.js
+++ client/components/report/TopTagsPanel.js
@@ -1,84 +1,27 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
-import PieChart from "react-svg-piechart";
-
-const COLORS = ["#3EABAB", "#FF96AC", "#3B5F5F", "#F3BBC8", "#FAE6EB"];
+import TopTagsChart from "./TopTagsChart";
 
 export default class TopTagsPanel extends Component {
   static propTypes = {
     tags: PropTypes.array.isRequired
   };
 
-  constructor() {
-    super();
-    this.state = { rendered: false };
-  }
-
   componentDidMount() {
-    this.setState({ rendered: true });
+    this.chart = new TopTagsChart(this.canvas, this.props);
   }
 
   render() {
     return (
       <div className="TopTagsPanel ReportElement">
+        <p className="TopTagsPanel-title">Kehutuimmat taidot</p>
         <div className="TopTagsPanel-wrapper">
-          <PieChart
-            data={this.getData()}
-            transitionDuration=".2s"
-            transitionTimingFunction="ease-in-out"
+          <canvas
+            className="TopTagsPanel-canvas"
+            ref={ref => (this.canvas = ref)}
           />
         </div>
-        {this.renderLabels()}
       </div>
     );
-  }
-
-  getData() {
-    return this.props.tags.map((tag, i) => ({
-      value: tag.count,
-      title: tag.text,
-      color: COLORS[i]
-    }));
-  }
-
-  renderLabels() {
-    return this.props.tags.map((tag, i) => {
-      if (!this.state.rendered) {
-        return null;
-      }
-      const elem = document.querySelector(`[fill="${COLORS[i]}"]`);
-      const rect = elem.getBoundingClientRect();
-      const parentRect = elem.parentElement.getBoundingClientRect();
-      const relativeTop =
-        rect.top + window.screenY - (parentRect.top + window.screenY);
-      const relativeLeft =
-        rect.left + window.screenX - (parentRect.left + window.screenX);
-
-      let style = {
-        position: "absolute"
-      };
-
-      if (rect.top === parentRect.top) {
-        style.top = rect.height / 3;
-      } else if (rect.bottom === parentRect.bottom) {
-        style.bottom = rect.height / 3;
-      } else {
-        style.top = relativeTop + rect.height / 2;
-      }
-
-      if (rect.left === parentRect.left) {
-        style.left = rect.width / 3;
-      } else if (rect.right === parentRect.right) {
-        style.right = rect.width / 3;
-      } else {
-        style.left = relativeLeft + rect.width / 3;
-      }
-
-      return (
-        <span key={i} className="TopTagsPanel-label" style={style}>
-          {tag.text}
-        </span>
-      );
-    });
   }
 }
