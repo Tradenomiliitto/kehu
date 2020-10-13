8063e38aa3212305aa25c6239b3744384b81bc9c
Samuli Asmala <samuli.asmala@gmail.com>
2020-10-13 20:15:04 +0300

    Files required to extract i18n keys from React app

--- client/index.js
+++ client/index.js
@@ -4,5 +4,6 @@ import App from "./App";
 import React from "react";
 import ReactDOM from "react-dom";
 import "./i18n";
+import "../dummy";
 
 ReactDOM.render(<App />, document.getElementById("app"));
---
+++ dummy.js
@@ -0,0 +1,335 @@
+t(
+  "translation-public:terms.10-text",
+  "Palveluun kirjaudutaan käyttäjän henkilökohtaisten sähköposti-, Google-, tai Facebook-tunnusten avulla. Palveluntarjoaja käsittelee Asiakkaan henkilötietoja Palvelun rekisteriselosteen sekä yksityisyydensuojaa koskevan lainsäädännön mukaisesti."
+);
+t("translation-public:terms.10-subtitle", "10 Henkilötietojen käyttö");
+t(
+  "translation-public:terms.09-text",
+  "Palveluntarjoaja käyttää palvelun ylläpitämisessä ja jatkokehityksessä alihankintana Rebase Interactiven tarjoamia palveluita. Alihankkijoilla on oikeus käsitellä kaikkia käyttäjien palveluun syöttämiä tietoja palvelun ylläpidon ja jatkokehittämisen edellyttämässä laajuudessa."
+);
+t("translation-public:terms.09-subtitle", "9 Alihankkijat");
+t(
+  "translation-public:terms.08f-text",
+  "Palveluntarjoaja ei ole miltään osin vastuussa käyttäjien luomasta sisällöstä. Mikäli käyttäjä kohtaa häirintää tai muuta asiatonta käytöstä toisen käyttäjän toimesta, on siitä viipymättä ilmoitettava Palveluntarjoajalle osoitteeseen toimisto@tral.fi."
+);
+t(
+  "translation-public:terms.08e-text",
+  "Palvelu sisältää linkkejä myös muille kuin Palveluntarjoajan ylläpitämille verkkosivuille. Palveluntarjoaja ei kontrolloi tällaisia sivuja eikä ole miltään osin vastuussa niiden sisällöstä tai laillisuudesta."
+);
+t(
+  "translation-public:terms.08d-text",
+  "Palveluntarjoaja ei vastaa käyttäjälle tai kolmannelle osapuolelle palvelun mahdollisesti sisältämistä virheistä tai puutteellisuuksista eikä palvelun sisältämien tietojen käytöstä tai tulkinnasta aiheutuvista välittömistä tai välillisistä vahingoista. Palveluntarjoaja ei myöskään vastaa palvelun soveltuvuudesta ja sopivuudesta mihinkään tiettyyn tarkoitukseen."
+);
+t(
+  "translation-public:terms.08c-text",
+  "Palveluntarjoaja ei vastaa mistään vahingosta, joka voi johtua palvelun toiminnasta tai siitä ettei palvelu toimi."
+);
+t(
+  "translation-public:terms.08b-text",
+  "Palveluntarjoaja ei vastaa palvelun mahdollisista toimintakatkoksista esimerkiksi huoltotöiden, teknisten vikojen tai muiden vastaavien syiden vuoksi eikä tietoliikenneyhteyksien tai internet-verkon mahdollisista toimintakatkoista."
+);
+t(
+  "translation-public:terms.08a-text",
+  "Palveluntarjoaja ei takaa, että palvelu on käytettävissä keskeytyksittä tai että se on virheetön."
+);
+t("translation-public:terms.08-subtitle", "8 Palveluntarjoajan vastuu");
+t(
+  "translation-public:terms.07-text",
+  "Käyttäjä vastaa siitä, että hän antaa palvelussa oikeat tiedot."
+);
+t("translation-public:terms.07-subtitle", "7 Käyttäjän vastuu");
+t(
+  "translation-public:terms.06c-text",
+  "Kehu kokonaisuudessaan, sen sisältämät aineistot ja tietokoneohjelmat ovat tekijänoikeuslain 1 §:ssä mainittuja tekijänoikeudellisesti suojattuja teoksia, joiden tekijänoikeus kuuluu tekijöille."
+);
+t(
+  "translation-public:terms.06b-text",
+  "Käyttäjällä ei ole oikeutta toisintaa tai kopioida palvelua tai sen sisältämää tietokantaa millään tavalla tai missään muodossa. Käyttäjällä ei myöskään ole oikeutta kerätä Kehun osia ja muodostaa niistä omaa palvelu-/aineistokokonaisuutta."
+);
+t(
+  "translation-public:terms.06a-text",
+  "Palvelu on tarkoitettu käyttäjän henkilökohtaiseen käyttöön. Käyttäjä ei saa luovuttaa palvelun käyttöoikeutta miltään osin edes väliaikaisesti kolmannelle osapuolelle."
+);
+t(
+  "translation-public:terms.06-subtitle",
+  "6 Palvelun käyttö ja tekijänoikeudet"
+);
+t(
+  "translation-public:terms.05b-text",
+  "Käyttäjä vastaa kaikesta käyttäjätunnuksellaan ja salasanallaan tapahtuneesta palvelun käytöstä sekä vahingosta ja kuluista, joita Palveluntarjoajalle aiheutuu sen johdosta, että kolmas osapuoli on saanut tunnukset haltuunsa."
+);
+t(
+  "translation-public:terms.05a-text",
+  "Käyttäjät rekisteröityvät Kehuun omalla sähköpostilla ja valitsemallaan salasanalla tai Facebook- tai Google-tunnuksilla Auth0-palvelun kautta. Käyttäjä on velvollinen huolehtimaan käyttäjätunnuksen ja salasanan huolellisesta säilyttämisestä. Jos käy ilmi tai on syytä epäillä, että kolmas osapuoli on saanut käyttäjän tunnukset haltuunsa, käyttäjän on välittömästi otettava yhteyttä osoitteeseen toimisto@tral.fi. Palveluntarjoajalla on myös oikeus poistaa kyseisiin tunnuksiin liittyvät käyttöoikeudet ja ryhtyä harkintansa mukaan muihin tarvittaviin toimenpiteisiin vahinkojen välttämiseksi."
+);
+t("translation-public:terms.05-subtitle", "5 Käyttäjätunnukset ja salasanat");
+t(
+  "translation-public:terms.04-text",
+  "Kehun käyttämiseen tarvitaan internet-yhteys ja verkkoselain. Käyttäjä vastaa itse Kehun käytön edellyttämän laitteiston ja yhteyksien hankinnasta sekä muista palvelun käyttöön liittyvistä teknisistä laitteista ja niihin liittyvistä kustannuksista."
+);
+t("translation-public:terms.04-subtitle", "4 Laitteistovaatimukset");
+t(
+  "translation-public:terms.03-text",
+  "Palvelu on oman osaamisen tunnistamiseen ja urakehitykseen luotu työkalu, jossa käyttäjät voivat luoda profiilin, tallentaa, vastaanottaa ja lähettää kehuja, käsitellä omia kehuja ja luoda raportteja. Palvelu sisältää myös blogin, jonka lukeminen ei edellytä rekisteröitymistä."
+);
+t("translation-public:terms.03-subtitle", "3 Palvelun sisältö");
+t("translation-public:terms.02-text", {
+  link: `<a href="/${language}/blogi">`,
+  "end-link": "</a>",
+  defaultValue:
+    "Palveluntarjoajalla on oikeus muuttaa näitä käyttöehtoja. Olennaisista käyttöehtojen muutoksista tiedotetaan Kehun omassa {{link}}blogissa{{end-link}}. Muutokset astuvat voimaan ilmoitettuna ajankohtana. Jatkamalla Kehun käyttöä ehtojen muuttumisen jälkeen käyttäjä sitoutuu noudattamaan muuttuneita ehtoja."
+});
+t(
+  "translation-public:terms.02-subtitle",
+  "2 Muutokset käyttöehdoissa ja palvelussa"
+);
+t(
+  "translation-public:terms.01-text",
+  "Tämä käyttäjäsopimus (sopimus) sisältää Tradenomiliitto TRAL:n ry:n (Palveluntarjoaja) ylläpitämän Kehu-palvelun (palvelu) käyttöä koskevat ehdot. Palvelun käyttäjäksi (käyttäjä) katsotaan jokainen, joka käyttää palvelua. Palvelu on avoin, joten kaikki halukkaat voivat luoda profiilin, tallentaa, lähettää ja vastaanottaa kehuja sekä luoda raportteja kehuistaan. Palvelu sisältää myös blogin, jonka lukeminen ei edellytä rekisteröitymistä."
+);
+t("translation-public:terms.01-subtitle", "1 Yleistä");
+t("translation-public:terms.title", "KEHU-PALVELUN KÄYTTÖEHDOT");
+t("translation-public:terms.page-description", "KEHU-palvelun käyttöehdot");
+t("translation-public:terms.page-title", "Kehu – Käyttöehdot");
+t("translation-public:login.signup-terms", {
+  defaultValue:
+    "Hyväksyn {{link1}}rekisteriselosteen{{end-link}} ja {{link2}}käyttöehdot{{end-link}}",
+  link1,
+  link2,
+  "end-link": "</a>"
+});
+t("translation-public:login.last-name", "Sukunimi");
+t("translation-public:login.first-name", "Etunimi");
+t(
+  "translation-public:login.redirect-to-login",
+  "Uudelleenohjataan kirjautumiseen..."
+);
+t(
+  "translation-public:privacy-policy.09-text",
+  "Rekisteri on suojattu ulkopuoliselta käytöltä ja henkilötietojen käyttöä valvotaan. Pääsy rekisteriin on sallittu rajatulle joukolle."
+);
+t(
+  "translation-public:privacy-policy.09-subtitle",
+  "9 Rekisterin suojauksen periaatteet"
+);
+t(
+  "translation-public:privacy-policy.08-text",
+  "Tietoja ei siirretä EU- tai ETA-maiden ulkopuolelle."
+);
+t(
+  "translation-public:privacy-policy.08-subtitle",
+  "8 Tietojen siirto EU:n tai ETA:n ulkopuolelle"
+);
+t(
+  "translation-public:privacy-policy.07b-text",
+  "Palvelun kehityksessä hyödynnetään Google Analytics –palvelua."
+);
+t(
+  "translation-public:privacy-policy.07a-text",
+  "Alihankkijalla, Rebase Interactive, on oikeus käsitellä käyttäjien palveluun syöttämiä tietoja."
+);
+t(
+  "translation-public:privacy-policy.07-subtitle",
+  "7 Tietojen säännönmukaiset luovutukset"
+);
+t(
+  "translation-public:privacy-policy.06b-text",
+  "KEHUun tallennetut tiedot eivät päivity Tradenomiliiton jäsenrekisteriin tai muihin kirjautumiseen käytettyihin palveluihin."
+);
+t(
+  "translation-public:privacy-policy.06a-text",
+  "Käyttäjien itse palveluun syöttämät tiedot."
+);
+t(
+  "translation-public:privacy-policy.06-subtitle",
+  "6 Säännönmukaiset tietolähteet"
+);
+t(
+  "translation-public:privacy-policy.05d-text",
+  "Kerättyä tietoa ylläpidetään Heroku-palvelussa."
+);
+t(
+  "translation-public:privacy-policy.05c-text",
+  "Käyttäjä määrittelee itse, mitä yhteystietoja haluaa jakaa ja kenelle."
+);
+t(
+  "translation-public:privacy-policy.05b-text",
+  "Käyttäjän nimi ja profiilikuva on saatavilla vain muille kirjautuneille käyttäjille."
+);
+t(
+  "translation-public:privacy-policy.05a-text",
+  "Käyttäjien palveluun syöttämät tiedot, kuten nimi ja yhteystiedot."
+);
+t("translation-public:privacy-policy.05-subtitle", "5 Rekisterin tietosisältö");
+t(
+  "translation-public:privacy-policy.04c-text",
+  "Henkilötietoja käsitellään henkilötietolain sallimissa ja velvoittamissa rajoissa palvelun tuottamisessa."
+);
+t("translation-public:privacy-policy.04b-text", "Palvelun jatkokehittäminen");
+t(
+  "translation-public:privacy-policy.04a-text",
+  "Jäsenpalvelujen toimittaminen"
+);
+t(
+  "translation-public:privacy-policy.04-subtitle",
+  "4 Henkilötietojen käsittelyn tarkoitus"
+);
+t(
+  "translation-public:privacy-policy.03a-text",
+  "KEHU–palvelun käyttäjärekisteri"
+);
+t("translation-public:privacy-policy.03-subtitle", "3 Rekisterin nimi");
+t("translation-public:privacy-policy.phone-abbreviation");
+t(
+  "translation-public:privacy-policy.02-subtitle",
+  "2 Yhteyshenkilö rekisteriä koskevissa asioissa"
+);
+t("translation-public:privacy-policy.phone-abbreviation", "Puh.");
+t("translation-public:privacy-policy.01-subtitle", "1 Rekisterinpitäjä");
+t("translation-public:privacy-policy.title", "REKISTERISELOSTE");
+t("translation-public:privacy-policy.page-description", "Rekisteriseloste");
+t("translation-public:privacy-policy.page-title", "Kehu – Rekisteriseloste");
+t(
+  "translation-public:error.javascript-required",
+  "Valitettavasti Kehu-sovellus ei toimi ilman JavaScriptia. Ota se käyttöön jatkaaksesi käyttöä."
+);
+t("translation-public:info.10-text", {
+  link: '<a href="mailto:toimisto@tral.fi">',
+  "end-link": "</a>",
+  defaultValue:
+    "Ongelmatilanteissa saat apua osoitteesta {{link}}toimisto@tral.fi{{end-link}}."
+});
+t(
+  "translation-public:info.09-text",
+  "Huomioithan, että voit jatkossa liittää Kehu-tiliisi myös muita käytössäsi olevia sähköpostiosoitteita. Kehujen lähettämisen yhteydessä toimitetaan vastaanottajalle aina myös sähköposti, josta löytyy ohjaus osoitteen liittämiseen omaan tiliin. Tämä tarkoittaa myös, että sinun ei tarvitse tietää, mitä sähköpostiosoitetta esimerkiksi kollegasi käyttää Kehu-tilillään, sillä kaikki vastaanotetut kehut on mahdollista liittää samalle tilille."
+);
+t(
+  "translation-public:info.08-text",
+  "Mikäli olet rekisteröitynyt Facebookin tai Googlen kautta, käythän lisäämässä oman sähköpostisi omaan profiiliisi, jotta muiden lähettämien kehujen vastaanottaminen olisi mahdollisimman vaivatonta."
+);
+t(
+  "translation-public:info.07-text",
+  "Blogista löydät vinkkejä mm. oman osaamisen tunnistamiseen, työhakuun, palautteen antamiseen, kehityskeskusteluihin sekä työhakemuksen ja CV:n laadintaan."
+);
+t(
+  "translation-public:info.06-subtitle",
+  "Mitä hyötyä kehujen keräämisestä on?"
+);
+t(
+  "translation-public:info.05-text",
+  "Maailmaan mahtuu aina lisää kannustavia sanoja. Levitä iloa ympärillesi kehumalla alaista tai kollegaa hyvin tehdystä työstä ja luo hyvää ilmapiiriä työyhteisöösi. Hyvän palautteen antaminen on helppoa - tarvitset vain kehuttavan henkilön sähköpostiosoitteen. Vastaanottajan ei tarvitse olla Kehun käyttäjä, hän saa sähköposti-ilmoituksen saamastaan kehusta joka tapauksessa. Lähettämällä kehun voit samalla kutsua uusia käyttäjiä!"
+);
+t(
+  "translation-public:info.04-subtitle",
+  "Lisää työhyvinvointia kehumalla kollegaa tai alaista"
+);
+t(
+  "translation-public:info.03-text",
+  "Kehussa vastaanottamasi tai sinne kirjaamasi hyvä palaute kertyy helppokäyttöiseksi tilastoksi, jota voit käyttää hyväksesi työpaikkaa hakiessa tai kehityskeskustelussa. Lisää taitosi ja vahvuutesi työhakemukseen tai CV:hen tai mainitse saamasi kehu työhaastattelussa!"
+);
+t(
+  "translation-public:info.02-subtitle",
+  "Kerää positiivinen palaute hyödynnettäväksi dataksi"
+);
+t(
+  "translation-public:info.01-text",
+  "Kehu on täysin ilmainen ja kaikille avoin palvelu, jonka avulla voit lähettää, vastaanottaa ja kirjata hyvää palautetta. Kehujen listaamisen kautta saat konkreettisen käsityksen omista taidoistasi ja vahvuuksistasi. Lisäksi voit levittää hyvää fiilistä ympärillesi helpolla tavalla. Palvelu on henkilökohtainen eikä lisäämäsi sisältö näy muille."
+);
+t("translation-public:info.00-title", "Opi tuntemaan vahvuutesi");
+t(
+  "translation-public:info.page-description",
+  "Kehu on täysin ilmainen ja kaikille avoin palvelu, jonka avulla voit lähettää, vastaanottaa ja kirjata hyvää palautetta. Kehujen listaamisen kautta saat konkreettisen käsityksen omista taidoistasi ja vahvuuksistasi. Lisäksi voit levittää hyvää fiilistä ympärillesi helpolla tavalla."
+);
+t("translation-public:info.page-title", "Kehu – Info");
+t("translation-public:frontpage.readblog-btn", "Lue blogia");
+t(
+  "translation-public:frontpage.title5-benefits-text",
+  "Blogista löydät vinkkejä mm. oman osaamisen tunnistamiseen, työhakuun, palautteen antamiseen, kehityskeskusteluihin sekä työhakemuksen ja CV:n laadintaan."
+);
+t(
+  "translation-public:frontpage.title5-benefits",
+  "Mitä hyötyä kehujen keräämisestä on?"
+);
+t("translation-public:frontpage.create-account-btn");
+t(
+  "translation-public:frontpage.title4-joinnow-text",
+  "Kun tunnet vahvuutesi, voit hyödyntää niitä työnhaussa ja kehityskeskusteluissa"
+);
+t(
+  "translation-public:frontpage.title4-joinnow",
+  "Ala kerryttämään dataa kehuistasi jo tänään"
+);
+t(
+  "translation-public:frontpage.title3-wellbeing-text",
+  "Maailmaan mahtuu aina lisää kannustavia sanoja. Levitä iloa ympärillesi kehumalla alaista tai kollegaa hyvin tehdystä työstä ja luo hyvää ilmapiiriä työyhteisöösi. Hyvän palautteen antaminen on helppoa - tarvitset vain kehuttavan henkilön sähköpostiosoitteen."
+);
+t(
+  "translation-public:frontpage.title3-wellbeing",
+  "Lisää työhyvinvointia kehumalla kollegaa tai alaista"
+);
+t(
+  "translation-public:frontpage.title2-data-text",
+  "Kehussa vastaanottamasi tai sinne kirjaamasi hyvä palaute kertyy helppokäyttöiseksi tilastoksi, jota voit käyttää hyväksesi työpaikkaa hakiessa tai kehityskeskustelussa. Lisää taitosi ja vahvuutesi työhakemukseen tai CV:hen tai mainitse saamasi kehu työhaastattelussa!"
+);
+t(
+  "translation-public:frontpage.title2-data",
+  "Kerää positiivinen palaute hyödynnettäväksi dataksi"
+);
+t("translation-public:frontpage.create-account-btn", "Luo tili");
+t(
+  "translation-public:frontpage.title-text",
+  "Kehu on täysin ilmainen ja kaikille avoin palvelu, jonka avulla voit lähettää, vastaanottaa ja kirjata hyvää palautetta. Kehujen listaamisen kautta saat konkreettisen käsityksen omista taidoistasi ja vahvuuksistasi. Lisäksi voit levittää hyvää fiilistä ympärillesi helpolla tavalla. Palvelu on henkilökohtainen eikä lisäämäsi sisältö näy muille."
+);
+t("translation-public:frontpage.title", "Opi tuntemaan vahvuutesi.");
+t("translation-public:error.text", {
+  status: error.status,
+  link: `<a href="/${language}">`,
+  "end-link": "</a>",
+  defaultValue:
+    "SOS koodisana {{status}}. Palaa takaisin edelliselle sivulle tai {{link}}navigoi etusivullemme{{end-link}}."
+});
+t("translation-public:error.title", "Täältä ei löydy mitään kehuttavaa");
+t(
+  "translation-public:blog.subtitle",
+  "Nappaa parhaat vinkit, miten kehut auttavat sinua urallasi eteenpäin"
+);
+t("translation-public:blog.title", "Kehun blogi");
+t(
+  "translation-public:blog.page-description",
+  "Lue blogeja työhyvinvoinnista, positiivisen palautteen vaikutuksesta, oman osaamisen tunnistamisesta ja sanoittamisesta sekä urasuunnittelusta."
+);
+t("translation-public:blog.page-title", "Kehu – Blogit");
+t("translation-public:blog.show-similar-posts", "Lue lisää aiheesta:");
+t("translation-public:blog.next-post", "Seuraava");
+t("translation-public:blog.previous-post", "Edellinen");
+t(
+  "translation-public:blog.browse-by-tags",
+  "Selaa postauksia aihepiireittäin:"
+);
+t("translation-public:navbar.login", "Kirjaudu");
+t("translation-public:navbar.blog");
+t("translation-public:navbar.kehus", "Kehut");
+t("translation-public:navbar.blog", "Blogi");
+t("translation-public:navbar.report", "Raportit");
+t("translation-public:navbar.send-kehu", "Lähetä Kehu");
+t("translation-public:navbar.add-kehu", "Lisää Kehu");
+t("translation-public:head.page-title");
+t("translation-public:head.page-title");
+t("translation-public:head.page-title", "Koe positiivisen palautteen voima!");
+t(
+  "translation-public:head.page-description",
+  "Kehu on täysin ilmainen ja kaikille avoin palvelu, jonka avulla voit lähettää, vastaanottaa ja kirjata hyvää palautetta. Koe positiivisen palautteen voima, ja anna ensimmäisen Kehusi jo tänään!"
+);
+t(
+  "translation-public:login.signup-terms-notice",
+  "Rekisteröityäksesi sinun tulee ensin hyväksyä rekisteriseloste ja käyttöehdot ikkunan alalaidasta."
+);
+t("translation-public:login.signup-terms", {
+  defaultValue:
+    "Hyväksyn {{link1}}rekisteriselosteen{{end-link}} ja {{link2}}käyttöehdot{{end-link}}",
+  link1,
+  link2,
+  "end-link": "</a>"
+});
+t("translation-public:login.last-name", "Sukunimi");
+t("translation-public:login.first-name", "Etunimi");
