c5f07c4b839987e9e3a4d14e9470ab10c5dc4dfb
Samuli Asmala <samuli.asmala@gmail.com>
2021-10-31 20:29:48 +0200

    Refactor Cloudinary upload

--- client/ProfilePanel.js
+++ client/ProfilePanel.js
@@ -5,8 +5,7 @@ import ProfileInfoPanel from "./components/profile/ProfileInfoPanel";
 import ProfileEditForm from "./components/profile/ProfileEditForm";
 import SuccessPanel from "./components/SuccessPanel";
 import { deleteProfile, updateProfile } from "./redux/profile";
-import { getText } from "./util/ApiUtil";
-import { toQueryString } from "./util/TextUtil";
+import { uploadWidget } from "./util/uploadWidget";
 import { compose } from "redux";
 import { withTranslation } from "react-i18next";
 
@@ -15,6 +14,9 @@ export class ProfilePanel extends Component {
     deleteProfile: PropTypes.func.isRequired,
     updateProfile: PropTypes.func.isRequired,
     profile: PropTypes.object.isRequired,
+    // i18n props
+    t: PropTypes.func.isRequired,
+    i18n: PropTypes.object.isRequired,
   };
 
   constructor() {
@@ -50,7 +52,7 @@ export class ProfilePanel extends Component {
                     </div>
                     <div className="UploadNewProfilePicture">
                       <button
-                        onClick={this.uploadWidget}
+                        onClick={this.handleUploadButtonClick}
                         className="Button upload-button"
                       >
                         {t("profile.change-picture-btn", "Vaihda profiilikuva")}
@@ -102,6 +104,21 @@ export class ProfilePanel extends Component {
     );
   }
 
+  handleUploadButtonClick = () => {
+    uploadWidget(
+      "profile_" + this.props.profile.auth0_id,
+      this.props.i18n.language,
+      this.pictureUploadCb
+    );
+  };
+
+  pictureUploadCb = async (url) => {
+    // Update profile picture if upload was succesful
+    if (url != null) {
+      this.props.updateProfile({ picture: url }, true);
+    }
+  };
+
   renderSuccessPanel() {
     const { t } = this.props;
     if (this.state.success) {
@@ -128,54 +145,6 @@ export class ProfilePanel extends Component {
     }
     return <ProfileInfoPanel profile={this.props.profile} />;
   }
-
-  uploadWidget = () => {
-    cloudinary.openUploadWidget(
-      {
-        cloudName: process.env.REACT_APP_CLOUDINARY_CLOUD_NAME,
-        uploadPreset: process.env.REACT_APP_CLOUDINARY_UPLOAD_PRESET,
-        apiKey: process.env.REACT_APP_CLOUDINARY_API_KEY,
-        cropping: true,
-        croppingAspectRatio: 1,
-        showSkipCropButton: false,
-        //croppingCoordinatesMode: 'face', // signature generation not working for some reason when using face mode
-        multiple: false,
-        publicId: "profile_" + this.props.profile.auth0_id,
-        uploadSignature: this.generateSignature,
-        language: this.props.i18n.language,
-        styles: {
-          palette: {
-            window: "#FFFFFF",
-            windowBorder: "#CFCFCF",
-            tabIcon: "#FF96AC",
-            menuIcons: "#5A616A",
-            textDark: "#000000",
-            textLight: "#FFFFFF",
-            link: "#FF96AC",
-            action: "#FF96AC",
-            inactiveTabIcon: "#3B5F5F",
-            error: "#ff5f83",
-            inProgress: "#0078FF",
-            complete: "#DBFFE5",
-            sourceBg: "#EDF1F1",
-          },
-        },
-      },
-      async (error, result) => {
-        // Update profile picture if upload was succesful
-        if (result && result.event === "success") {
-          this.props.updateProfile({ picture: result.info.secure_url }, true);
-        }
-      }
-    );
-  };
-
-  generateSignature = async (callback, params_to_sign) => {
-    let path = "/profiili/cloudinary-signature?";
-    let params = toQueryString({ data: params_to_sign });
-    let signature = await getText(encodeURI(path + params));
-    callback(signature);
-  };
 
   handleSuccess = () => {
     this.setState({
