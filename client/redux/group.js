a9ba65a8cf5c067b0a60a52c9845d86356ba117e
Samuli Asmala <samuli.asmala@gmail.com>
2021-12-04 17:33:00 +0200

    Add redux action creators and reducer for get and create groups

---
+++ client/redux/group.js
@@ -0,0 +1,81 @@
+import { get, post } from "../util/ApiUtil";
+
+export const GET_GROUPS = "groups/GET_GROUPS";
+export const GET_GROUPS_SUCCESS = "groups/GET_GROUPS_SUCCESS";
+export const GET_GROUPS_ERROR = "groups/GET_GROUPS_ERROR";
+
+export const CREATE_GROUP = "group/CREATE_GROUP";
+export const CREATE_GROUP_SUCCESS = "group/CREATE_GROUP_SUCCESS";
+export const CREATE_GROUP_ERROR = "group/CREATE_GROUP_ERROR";
+
+export const initialState = {
+  error: null,
+  loading: false,
+  groups: [],
+  groupsLoaded: false,
+};
+
+export function getGroups() {
+  return async (dispatch) => {
+    try {
+      dispatch({ type: GET_GROUPS });
+      const payload = await get("/yhteisot");
+      dispatch({ type: GET_GROUPS_SUCCESS, payload });
+    } catch (e) {
+      dispatch({ type: GET_GROUPS_ERROR, payload: e });
+    }
+  };
+}
+
+export function createGroup(data) {
+  return async (dispatch) => {
+    try {
+      dispatch({ type: CREATE_GROUP });
+      const group = await post("/yhteisot", data);
+      dispatch({ type: CREATE_GROUP_SUCCESS, payload: group });
+    } catch (e) {
+      dispatch({ type: CREATE_GROUP_ERROR, payload: e });
+    }
+  };
+}
+
+export default function reducer(state = initialState, action = {}) {
+  switch (action.type) {
+    case GET_GROUPS:
+    case CREATE_GROUP:
+      return {
+        ...state,
+        loading: true,
+        error: null,
+      };
+
+    case GET_GROUPS_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        error: null,
+        groups: action.payload,
+        groupsLoaded: true,
+      };
+
+    case CREATE_GROUP_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        error: null,
+        groups: [...state.groups, action.payload],
+        groupsLoaded: true,
+      };
+
+    case GET_GROUPS_ERROR:
+    case CREATE_GROUP_ERROR:
+      return {
+        ...state,
+        loading: false,
+        error: action.payload,
+      };
+
+    default:
+      return state;
+  }
+}
--- client/redux/index.js
+++ client/redux/index.js
@@ -2,12 +2,14 @@ import { createStore, combineReducers, applyMiddleware } from "redux";
 import logger from "redux-logger";
 import thunk from "redux-thunk";
 import kehu from "./kehu";
+import group from "./group";
 import portal from "./portal";
 import profile from "./profile";
 import report from "./report";
 
 const rootReducer = combineReducers({
   kehu,
+  group,
   portal,
   profile,
   report,
