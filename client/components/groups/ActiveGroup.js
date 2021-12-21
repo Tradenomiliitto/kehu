0eb5604c9ad9a9f39fa9854fd4dcb9423693f861
Samuli Asmala <samuli.asmala@gmail.com>
2021-12-21 21:23:19 +0200

    Make ActiveGroup component responsive

--- client/components/groups/ActiveGroup.js
+++ client/components/groups/ActiveGroup.js
@@ -11,25 +11,29 @@ export default function ActiveGroup({ group }) {
   const dispatch = useDispatch();
 
   const admins = group.members.filter((m) => m.is_admin);
-
   return (
-    <div className="Card MyGroups-ActiveGroup">
-      <div className="MyGroups-ActiveGroupPicture">
-        <img className="GroupPicture-image" src={group.picture} />
+    <div className="MyGroups-Card row">
+      <div className="col col-xs-12 col-sm-3 ActiveGroup-PictureContainer">
+        <div className="ActiveGroup-Picture">
+          <img className="GroupPicture-image" src={group.picture} />
+        </div>
       </div>
-      <div className="MyGroups-ActiveGroupDetails">
+
+      <div className="col col-xs-12 col-sm-9 ActiveGroup-Details">
         <h2>{group.name}</h2>
-        <div className="MyGroups-ActiveGroupDescription">
+        <div className="ActiveGroup-Description">
           {group.description || "\u00A0"}
         </div>
+
         <div>
           {group.members.map((member) => (
-            <div key={member.user.id} className="MyGroups-ActiveMemberPicture">
+            <div key={member.user.id} className="ActiveGroup-MemberPicture">
               <img className="MemberPicture-image" src={member.user.picture} />
             </div>
           ))}
         </div>
-        <div className="MyGroups-ActiveGroupInfo">
+
+        <div className="ActiveGroup-Info">
           {t("groups.member-count", {
             count: group.members.length,
             defaultValue: "{{count}} jäsen",
@@ -37,24 +41,41 @@ export default function ActiveGroup({ group }) {
           })}{" "}
           &#8211; {t("groups.joined-group", "liityit yhteisöön")}{" "}
           {moment(group.joined_at).format("D.M.YYYY")}
-          <br />
-          {t("groups.admin", {
-            count: admins.length,
-            defaultValue: "Admin",
-            defaultValue_plural: "Adminit",
-          })}
-          :{" "}
-          {admins
-            .map((m) => `${m.user.first_name} (${m.user.email})`)
-            .join(", ")}
+        </div>
+
+        <div className="row">
+          <div className="col col-xs-12 col-md-5 ActiveGroup-Info">
+            <div>
+              {t("groups.admin", {
+                count: admins.length,
+                defaultValue: "Admin",
+                defaultValue_plural: "Adminit",
+              })}
+              :{" "}
+              {admins
+                .map((m) => `${m.user.first_name} (${m.user.email})`)
+                .join(", ")}
+            </div>
+          </div>
+
+          <div className="col col-xs-12 col-md-7">
+            <div className="ActiveGroup-Buttons">
+              <button
+                className="Button Button--inverse"
+                onClick={() => dispatch(toggleSendKehuFormModal())}
+              >
+                {t("groups.edit-group-btn", "Muokkaa yhteisöä")}
+              </button>
+              <button
+                className="Button"
+                onClick={() => dispatch(toggleSendKehuFormModal())}
+              >
+                {t("home.send-new-kehu-btn", "Lähetä Kehu")}
+              </button>
+            </div>
+          </div>
         </div>
       </div>
-      <button
-        className="Button MyGroups-SendKehuBtn"
-        onClick={() => dispatch(toggleSendKehuFormModal())}
-      >
-        {t("home.send-new-kehu-btn", "Lähetä Kehu")}
-      </button>
     </div>
   );
 }
