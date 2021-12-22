aa22e227a35c4f8a54fdbea0837f4b06247b617f
Samuli Asmala <samuli.asmala@gmail.com>
2021-12-22 08:58:17 +0200

    Update stylelint to v14

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,7 +1,12 @@
 module.exports = {
   extends: [
-    "stylelint-config-standard",
+    "stylelint-config-standard-scss",
     "stylelint-config-sass-guidelines",
     "stylelint-prettier/recommended",
   ],
+  rules: {
+    "selector-class-pattern": null,
+    "scss/at-extend-no-missing-placeholder": null,
+    "color-named": null,
+  },
 };
