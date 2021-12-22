8f2aea1de5b907ccc3b53a00b70655fec5ed28fd
Samuli Asmala <samuli.asmala@gmail.com>
2021-12-22 11:39:08 +0200

    Remove unused url-loader

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -58,14 +58,6 @@ module.exports = {
         ],
       },
       {
-        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader: "url-loader",
-        options: {
-          limit: 10000,
-          mimetype: "application/font-woff",
-        },
-      },
-      {
         // Images are not imported in React app but provided as strings so we
         // cannot include then in Webpack bundle
         test: /\.(ttf|eot|svg|otf|png)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
