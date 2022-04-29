978d346a8a436ebeedfe52144e82fbf6970324a7
Samuli Asmala <samuli.asmala@gmail.com>
2022-04-29 13:42:47 +0300

    Add group kehu listing with paging

--- client/GroupsPanel.js
+++ client/GroupsPanel.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSelector } from "react-redux";
+import { useTranslation } from "react-i18next";
 
 import NoGroups from "./components/groups/NoGroups";
 import MyGroups from "./components/groups/MyGroups";
@@ -7,7 +8,11 @@ import FeedPanel from "./components/home/FeedPanel";
 import GroupMembers from "./components/groups/GroupMembers";
 import ActiveGroup from "./components/groups/ActiveGroup";
 
+const SHOW_MORE_KEHUS_STEP_SIZE = 5;
+
 export default function GroupsPanel() {
+  const [t] = useTranslation();
+  const [kehusVisible, setKehusVisible] = useState(SHOW_MORE_KEHUS_STEP_SIZE);
   const groups = useSelector((state) => state.group.groups);
   const activeGroupIdx = useSelector((state) => state.group.activeGroupIdx);
 
@@ -15,14 +20,23 @@ export default function GroupsPanel() {
 
   const activeGroup = groups[activeGroupIdx ?? 0];
 
-  const dummyFeedItem = [{ id: 1, text: "TODO", giver_name: "" }];
+  const showMoreKehus = (ev) => {
+    ev.target.blur();
+    setKehusVisible((kehusVisible) => kehusVisible + SHOW_MORE_KEHUS_STEP_SIZE);
+  };
+
   return (
     <div className="Groups">
       <div className="container">
         <div className="row">
           <div className="col col-xs-12 col-md-9">
             <ActiveGroup group={activeGroup} />
-            <FeedPanel items={dummyFeedItem} />
+            <FeedPanel items={activeGroup.kehus.slice(0, kehusVisible)} />
+            {activeGroup.kehus.length > kehusVisible && (
+              <button className="Button" onClick={showMoreKehus}>
+                {t("groups.show-more-kehus-btn", "Näytä lisää")}
+              </button>
+            )}
           </div>
           <div className="col col-xs-12 col-md-3">
             <MyGroups groups={groups} activeGroupIdx={activeGroupIdx} />
