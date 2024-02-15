573f3d74275b4272f64237d8edd32ea227505b64
Samuli Asmala <samuli.asmala@gmail.com>
2024-02-15 20:40:25 +0200

    Add logger to login and logout endpoints

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const passport = require("passport");
+const logger = require("../logger");
 
 router.get("/kirjaudu-ulos", async (req, res) => {
+  logger.info(`Logging out user ${req.user?.id}`);
   await new Promise((resolve, reject) => {
     req.logout({}, (err) => {
       if (err) reject(err);
@@ -16,6 +18,7 @@ router.get(
   "/callback",
   passport.authenticate("auth0", { failureRedirect: "/" }),
   (req, res) => {
+    logger.info(`User ${req.user?.id} logged in successfully`);
     const returnTo = req.session.returnTo;
     delete req.session.returnTo;
     res.redirect(returnTo || req.session.languageRedirect || "/");
