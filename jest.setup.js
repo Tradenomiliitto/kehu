2abefe8328a6f8cb5dbf82830e2fbde29e1410ef
Samuli Asmala <samuli.asmala@gmail.com>
2025-12-30 21:28:50 +0200

    Add TextEncoder/TextDecoder polyfill for Jest tests

--- jest.setup.js
+++ jest.setup.js
@@ -1,4 +1,5 @@
 import "@babel/polyfill";
+import { TextEncoder, TextDecoder } from "util";
 import React from "react";
 import fetch from "jest-fetch-mock";
 import Enzyme, { shallow } from "enzyme";
@@ -6,6 +7,8 @@ import Adapter from "enzyme-adapter-react-16";
 
 Enzyme.configure({ adapter: new Adapter() });
 
+global.TextEncoder = TextEncoder;
+global.TextDecoder = TextDecoder;
 global.React = React;
 global.shallow = shallow;
 global.fetch = fetch;
